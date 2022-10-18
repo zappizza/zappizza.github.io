1b3f83054d42f00c59ab5b75c7382bc2c3fdb4a0
RenanBa <renanbarbosadesouza@gmail.com>
2022-10-17 21:37:50 -0700

    Create function for services and project

--- js/application.js
+++ js/application.js
@@ -1,72 +1,53 @@
 $(document).ready(function(){
   console.log("Ready");
+  console.log(decodeURI(window.location.href.split("/").pop()));
+  var pageLocation = decodeURI(window.location.href.split("/").pop());
 
-  var collections_obj = null
+
 
   function myFunction(items) {
-    // console.log(items["categories"])
+    if (pageLocation == "index.html") {
+      categories(items)
+      projects(items)
+    }
+    else if (pageLocation.includes("#")){
+      console.log("icludes!!!!")
+    }
+  }
+
+  function projects(items) {
+    console.log(items["projects"])
+    for (var key in items["projects"]){
+    }
+  };
+
+  function categories(items) {
+    console.log(items["categories"])
     for (var key in items["categories"]){
-      // console.log(key);
-      console.log(items["categories"][key]["title"])
-      console.log(items["categories"][key])
       var len = items["categories"][key]["photos_ids"].length
-      console.log(items["categories"][key]["photos_ids"][len - 1])
-      photo_id = items["categories"][key]["photos_ids"][len - 1]
-      console.log(items["photos"])
+      photo_id = items["categories"][key]["photos_ids"][len - 1] - 1
+
+      var test_item_one = document.createElement('div');
+      test_item_one.className = "col-md-6 col-lg-4";
+      test_item_one.setAttribute("id", items["categories"][key]["title"]);
 
 
-      for (var photo_key in items["photos"]){
-        // if (photo_id == items["photos"][photo_key]["id"]) {
-        //   console.log(items["photos"][photo_key]["path"])
-        //   img.src = items["photos"][photo_key]["path"];
-        // };
-
-
-        var test_item_one = document.createElement('div');
-        test_item_one.className = "col-md-6 col-lg-4";
-
-        test_item_one.innerHTML = `<a href="projects.html">
-            <div class="portfolio-item mx-auto" >
+      test_item_one.innerHTML = `<a href="projects.html#${items["categories"][key]["title"]}">
+          <div class="portfolio-item mx-auto" >
+            <h3 class="first-txt text-uppercase">${items['categories'][key]['title']}</h3>
+            <img class="img-fluid" src="${items["photos"][photo_id]["path"]}" alt="obras">
+            <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
               <h3 class="first-txt text-uppercase">${items['categories'][key]['title']}</h3>
-              <img class="img-fluid" src="${items["photos"][photo_key]["path"]}" alt="obras">
-              <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
-                <h3 class="first-txt text-uppercase">${items['categories'][key]['title']}</h3>
-              </div>
             </div>
-          </a>`
-        document.getElementById('portfolio-grid').appendChild(test_item_one);
-      } // end of photo for
+          </div>
+        </a>`
+      document.getElementById('portfolio-grid').appendChild(test_item_one);
     } // end of categories for
-  } // end of myFunction
-
-
-  console.log(collections_obj)
+  } // end of categories function
 
 
 
 
-  // var settings = {
-  //   "url": "https://radiant-journey-86877.herokuapp.com/photos",
-  //   "method": "GET",
-  //   "timeout": 0,
-  //   "headers": {
-  //     "X-Requested-With": "XMLHttpRequest"
-  //   },
-  // };
-
-  // $.ajax(settings).done(function (response) {
-  //   // console.log(response);
-  //   myFunction(response);
-  // });
-});
-
-
-collections(result => {
-  collections_obj = result;
-});
-
-function collections (callback) {
-  var api_response = null;
   var settings = {
     "url": "https://radiant-journey-86877.herokuapp.com/photos",
     "method": "GET",
@@ -74,12 +55,15 @@ function collections (callback) {
     "headers": {
       "X-Requested-With": "XMLHttpRequest"
     },
-    success: callback
   };
 
   $.ajax(settings).done(function (response) {
-    return response;
+    // console.log(response);
+    myFunction(response);
   });
-}
+});
 
+function testFunction() {
+  console.log("testing works");
+}
 
